15bfc121b9908dfd3cb64c75395aaf2d99d8b1f4
0AkshathaV <akshatha.v@s.amity.edu>
2025-03-26 13:46:52 +0530

    case1

--- switch.c
+++ switch.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 int main(){
     int choice;
+   
+    while(1){
+    printf("1-> B-tech CSE");
+    printf("2-> B-tech AIML");
+    printf("3-> B-tech ECE");
+    printf("4-> B-tech MEC");
 
+    switch(choice){
+Case1:
+	    printf("B-tech CSE");
+    }break;
